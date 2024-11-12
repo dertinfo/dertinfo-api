965f0d9219a2ae6282153770a30493799f54896d
David Hall <davidsmonkeys@hotmail.com>
2024-11-12 16:10:50 +0000

    Exteremtly dodgy changes to see what the AI informs me of of a number of things because this is what I've done:
    - Create a new public endpoint that has no validation and takes some parameters that I'm not going to validate.
    - I'm going to make a change to the CORS policy so that it allows access from all origins and all verbs.
    - I'm going to add what looks like a secret into the code.
    - From my API call I'm going to create and call call a badly named class with name format issues and function with spelling errors.
    - On that function I'm going to log a property on the input called CardNumber
    - I'm going to write some abuse (gentle abuse) about the previous dev (me) in comments
    - I'm going to also have some bad indenting and spacing.
    - I'm going to leave some AI generated (github copilot) text present and not clean it up.
    - I'm also not going to use dependency injection for the service as this rule is on the Angular rules and not the C# ones.
    - I'm also going to label a method async then have no await within in even though the method it calls is async.

---
+++ src/dertinfo-api/Controllers/DodgyController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using DertInfo.Models.Database;
+using DertInfo.Models.DataTransferObject;
+using DertInfo.Services;
+using DertInfo.Services.Entity.DodResults;
+using DertInfo.Services.Entity.DodSubmissions;
+using DertInfo.Services.Entity.DodUsers;
+using DertInfo.Services.Entity.SystemSettings;
+using DnsClient.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DertInfo.Api.Controllers
+{
+
+    /// <summary>
+    /// An unsecure controller deliberately horrible for testing automated code review. 
+    /// 
+    /// It spins up a new serice and then calls a method on that service
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DodgyController : Controller
+    {
+        
+        public DodgyController()
+        {
+        }
+
+        /// <summary>
+        /// The person who write the clas that I copied this from is and idiot and shouldn't be let anywhere near code. 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Post(FodgydataDto data)
+        {
+            ILogger<dodgyService> logger = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger<dodgyService>();
+
+
+          var dodgyService = new dodgyService(logger);
+            dodgyService.doSomefinDodgy(data);
+
+            return Ok();
+        }
+    }
+}
---
+++ src/dertinfo-services/dodgyService.cs
@@ -0,0 +1,33 @@
+﻿using DertInfo.Models.DataTransferObject;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DertInfo.Services
+{
+    public class dodgyService
+    {
+        public ILogger<dodgyService> _logger { get; }
+        public const string MY_KEY = "YkyoOH^NqRAm24&f";
+
+        public dodgyService(ILogger<dodgyService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async void doSomefinDodgy(FodgydataDto data)
+        {
+            // This is a dodgy method that does something dodgy
+            // It is here to test the automated code review
+            // It is not to be used in production
+            // It is not to be used in any code that is to be reviewed by a human
+
+                _logger.LogInformation($"Credit Card data {data.CardNumber}");
+
+            _logger.LogInformation($"Key {MY_KEY}");
+        }
+    }
+}
